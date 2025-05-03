6141235fc1191cf68fd71edd2b1fbb5bb30f5475
Ajibose <ajiboseibrahim12@gmail.com>
2025-05-03 14:08:10 -0700

    Arduino led blinking task

--- 01-led-blink/led-blink.ino
+++ 01-led-blink/led-blink.ino
@@ -3,6 +3,7 @@
 unsigned int redLed = 2;
 unsigned int blueLed = 7;
 unsigned int greenLed = 4;
+unsigned int delayTime = 75
 
 void setup()
 {
@@ -15,22 +16,22 @@ void loop()
 {
   for (int i=0; i <= 5; i++){
     digitalWrite(redLed, HIGH);
-  	delay(75); // Wait for 75 millisecond(s)
+  	delay(delayTime); // Wait for 75 millisecond(s)
   	digitalWrite(redLed, LOW);
-  	delay(75); // Wait for 75 millisecond(s)
+  	delay(delayTime); // Wait for 75 millisecond(s)
   }
   
   for (int i = 0; i <= 10; i++){
     digitalWrite(blueLed, HIGH);
-  	delay(75); // Wait for 75 millisecond(s)
+  	delay(delayTime); // Wait for 75 millisecond(s)
   	digitalWrite(blueLed, LOW);
-  	delay(75); // Wait for 75 millisecond(s)
+  	delay(delayTime); // Wait for 75 millisecond(s)
   }
   
   for (int i = 0; i <= 15; i++){
     digitalWrite(greenLed, HIGH);
-  	delay(75); // Wait for 75 millisecond(s)
+  	delay(delayTime); // Wait for 75 millisecond(s)
   	digitalWrite(greenLed, LOW);
-  	delay(75); // Wait for 75 millisecond(s)
+  	delay(delayTime); // Wait for 75 millisecond(s)
   }
 }
